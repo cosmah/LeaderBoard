7dc3b6296e43941ebfc88f6d65d7f8325de94144
cosmah <cosmahke4@gmail.com>
2024-02-06 23:17:24 +0300

    added themeleaf

--- project/src/main/java/backend/controller/PeopleController.java
+++ project/src/main/java/backend/controller/PeopleController.java
@@ -1,4 +1,12 @@
 package backend.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PeopleController {
+    @RequestMapping(value = "/index")
+    public String index(){
+        return "index";
+    }
 }
